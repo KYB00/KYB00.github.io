34db7d1e5ea66cfb00e80f91ef0e09ba83ffd98d
KYB00 <https://github.com/KYB00>
2024-06-18 23:29:16 +0900

    tab_update(보라색)

--- dist/js/tab.js
+++ dist/js/tab.js
@@ -1,9 +1,6 @@
-$(function(){
-  $('.tabcontent > div').hide();
-  $('.tabnav a').click(function () {
-    $('.tabcontent > div').hide().filter(this.hash).fadeIn();
-    $('.tabnav a').removeClass('active');
-    $(this).addClass('active');
-    return false;
-  }).filter(':eq(0)').click();
-  });
+var tabs = $('.tabs > li');
+
+tabs.on("click", function(){
+  tabs.removeClass('active');
+  $(this).addClass('active');
+});

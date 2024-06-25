382a83c87db87f70f87aa47bbf3d300e49d98df7
KYB00 <blu_dog_@naver.com>
2024-06-25 12:12:58 +0900

    포트폴리오 이미지 추가

--- dist/js/bodymovin.js
+++ dist/js/bodymovin.js
@@ -6,3 +6,54 @@ var animation = bodymovin.loadAnimation({
     autoplay: true, // Optional
     name: 'Hello World', // Name for future reference. Optional.
 })
+
+
+$(document).ready(function () {
+    $("#fullPage").fullpage({
+        // autoScrolling: true,
+        // scrollHorizontally: true,
+        navigation: true,
+        showActiveTooltip: true,
+        navigationTooltips: ["INTRO", "ABOUT ME", "PORTFOLIO", "MORE INFO"],
+        anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "fifthPage", "sixPage"],
+        afterLoad: function (anchorLink, index) {
+            if (index == 1) {
+                $(".home-wrap").css({
+                    width: "450px",
+                    height: "450px",
+                });
+                $(".home-wrap").animate(
+                    {
+                        opacity: 1,
+                    },
+                    100
+                );
+                $(".arrow").css({
+                    opacity: 1,
+                });
+            }
+        },
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 1 && direction == "down") {
+                $(".home-wrap").css({
+                    width: "4000px",
+                    height: "2000px",
+                });
+                $(".arrow").css({
+                    opacity: 0,
+                });
+            }
+            // if (nextIndex >= 3) {
+            //     $("#header").animate(
+            //         {
+            //             opacity: 1,
+            //         },
+            //         500
+            //     );
+            // } else {
+            //     $("#header").animate({
+            //         opacity: 0,
+            //     });
+            // }
+        },
+    });
